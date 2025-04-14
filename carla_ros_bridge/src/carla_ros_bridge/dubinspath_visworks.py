2590739457007353746b2b613c349c0650084601
Challen Adu <enninful@alphonsomonk.engin.umich.edu>
2025-04-14 17:44:44 -0400

    Ego vehicle waypoints and reach set visualization updated

--- carla_ros_bridge/src/carla_ros_bridge/dubinspath_visworks.py
+++ carla_ros_bridge/src/carla_ros_bridge/dubinspath_visworks.py
@@ -6,7 +6,7 @@ egg_path = '/home/carla/CarlaDepotAutomation/carla-0.9.14-py3.8-linux-x86_64.egg
 sys.path.append(egg_path)
 
 import carla
-
+import math
 import dubins
 import numpy as np
 from nav_msgs.msg import Odometry
@@ -21,7 +21,69 @@ class VehicleController:
         self.world = world  # Set the CARLA world object
         self.vehicle = None
         self.wheelbase = 3
-	
+
+    def draw_waypoints(self, waypoints, z=0, life_time=1.0, color=carla.Color(0, 255, 0)):
+        """
+        Draw a list of waypoints at a certain height given in z.
+
+            :param world: carla.world object
+            :param waypoints: list or iterable container with the waypoints to draw
+            :param z: height in meters
+        """
+        for wpt in waypoints:
+            # wpt_t = wpt.transform
+            wpt_t = wpt
+            # import pdb; pdb.set_trace()
+            begin = carla.Location(x=wpt_t[0], y=wpt_t[1], z=z)
+            angle = math.radians(wpt_t[2])
+            end = begin + carla.Location(x=math.cos(angle), y=math.sin(angle))
+            self.world.debug.draw_arrow(begin, end, arrow_size=0.3, life_time=life_time, color=color)
+
+    def draw_circles(self, waypoints, z=0, life_time=1.0, color=carla.Color(255, 255, 255)):
+        """
+        Draw a list of waypoints at a certain height given in z.
+
+            :param world: carla.world object
+            :param waypoints: list or iterable container with the waypoints to draw
+            :param z: height in meters
+        """
+        for wpt in waypoints:
+            wpt_t = wpt
+            center = [wpt_t[0], wpt_t[1], z]
+            self.draw_circle_from_center(center=center, radius=1.0, color=color)
+        
+        return
+    
+    def draw_circle_from_center(self, center, radius, life_time=10 , color=carla.Color(255, 255, 255)):
+        positions = []
+
+        num_points = 20
+        thetas = np.linspace(0, 2*np.pi, num_points)
+
+        for i in range(num_points):
+            positions.append((center[0] + radius*np.cos(thetas[i]),
+                            center[1] + radius*np.sin(thetas[i]),
+                            center[2]))        
+
+        for i in range(1, len(positions)):
+            self.world.debug.draw_line(
+                carla.Location(*positions[i-1]), 
+                carla.Location(*positions[i]), 
+                thickness=0.01, 
+                color=color, 
+                life_time=life_time
+            )
+
+        self.world.debug.draw_line(
+            carla.Location(*positions[-1]), 
+            carla.Location(*positions[0]), 
+            thickness=0.01, 
+            color=color, 
+            life_time=life_time
+        )
+        pass
+    
+	# DEPRECATED FUNCTION BELOW
     def draw_sphere(self, center, radius=1.0, num_points=50, color=carla.Color(255, 0, 0), life_time=0):
         """
         Draws a sphere by placing points in a spherical pattern and connects them to form a circle.
@@ -92,15 +154,16 @@ class VehicleController:
             rospy.loginfo("Vehicle pose received. Starting movement.")
 
             # Define the hardcoded waypoints (skip the first as instructed)
+            # Waypoints are in SE2 so (x,y,yaw)
             waypoints = [
-                (-28.96, -105.75),
-                (-14.68, -174.61),
-                (-86.83, -183.85),
-                (-88.23, -15.68),
-                (-51.66, -60.93),
-                (-10.15, -11.42),
-                (-7.34, -120.40),
-                (-62.91, -104.85)
+                (-30.96, 0.00, 0),
+                (-20.68, 0.00, 0),
+                (-10.00, 0.00, 0),
+                (0.00,  0.00, 0),
+                (10.00,  0.00, 0),
+                (20.00,  0.00, 0),
+                (30.00,  0.00, 0),
+                (40.00,  0.00, 0)
             ]
 
             turning_radius = 2.6  # Adjust as needed
@@ -109,15 +172,16 @@ class VehicleController:
             # Visualize the path waypoints as green dots
             for wp in waypoints:
                 #self.publish_view_approximation(wp[0], wp[1], 0, carla.Color(0, 255, 0),600)  # Green color for path waypoints
-                sphere_center = carla.Location(x=wp[0], y=wp[1], z=0.5)  # Center of the sphere
-                self.draw_sphere(sphere_center, radius=1.0, num_points=50,color=carla.Color(0, 255, 0),life_time=45)
+                sphere_center = carla.Location(x=wp[0], y=wp[1], z=0)  # Center of the sphere
+                self.draw_waypoints(waypoints, z=0, life_time=45, color=carla.Color(0, 255, 0))
+                # self.draw_sphere(sphere_center, radius=1.0, num_points=50,color=carla.Color(0, 255, 0),life_time=45)
 
             for i in range(len(waypoints) - 1):
             
                 start = waypoints[i] + (self.get_yaw_from_pose(self.current_pose),)
                 goal = waypoints[i + 1] + (0,)  # Orientation at goal can be 0 or any desired orientation
                 rospy.loginfo(f"Moving towards waypoint {i+1}: x={goal[0]}, y={goal[1]}")
-                rate.sleep()
+                # rate.sleep()
 
                 dubins_path = self.compute_dubins_path(start, goal, turning_radius)
                 
@@ -125,8 +189,9 @@ class VehicleController:
                 all_sphere_positions = []
                 for configuration in dubins_path:
                     x, y, yaw = configuration
-                    sphere_center = carla.Location(x=x, y=y, z=0.5)
-                    positions = self.draw_sphere(sphere_center, radius=1.0, num_points=50, color=carla.Color(157, 0, 255),life_time=10)
+                    sphere_center = carla.Location(x=x, y=y, z=0)
+                    positions = self.draw_circles(waypoints, z=0, life_time=1.0)
+                    # positions = self.draw_sphere(sphere_center, radius=1.0, num_points=50, color=carla.Color(157, 0, 255),life_time=10)
                     all_sphere_positions.append(positions)
 
                 for j, configuration in enumerate(dubins_path):
